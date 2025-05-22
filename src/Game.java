d254be6d52c9be3a12a104d4c14d63ad5f513e68
Admin <Admin@DESKTOP-LC407O9>
2025-05-22 20:44:32 +0200

    Dodanie klasy Player, aby odróżnić dwóch graczy - aktualizacja klasy Game o drugiego gracza.

---
+++ src/Game.java
@@ -0,0 +1,34 @@
+public class Game {
+    public static void main(String[] args) {
+        FirstThrow firstThrow = new FirstThrow();
+        SecondThrow secondThrow = new SecondThrow();
+
+        Player player1 = new Player("Gracz 1");
+        Player player2 = new Player("Gracz 2");
+
+        player1.firstThrow(firstThrow);
+
+        player2.firstThrow(firstThrow);
+
+        player1.secondThrow(secondThrow);
+
+        player2.secondThrow(secondThrow);
+
+        secondThrow.closeScanner();
+
+        System.out.println("\nWyniki końcowe:");
+
+        System.out.println(player1.getName() + ":");
+        printDice(player1.getDice());
+
+        System.out.println(player2.getName() + ":");
+        printDice(player2.getDice());
+    }
+
+    private static void printDice(int[] dice) {
+        for (int i = 0; i < dice.length; i++) {
+            System.out.print(dice[i] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,26 @@
+public class Player {
+    private String name;
+    private int[] dice = new int[5];
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int[] getDice() {
+        return dice;
+    }
+
+    public void firstThrow(FirstThrow firstThrow) {
+        System.out.println("\n" + name + " wykonuje pierwszy rzut:");
+        dice = firstThrow.roll();
+    }
+
+    public void secondThrow(SecondThrow secondThrow) {
+        System.out.println("\n" + name + " wykonuje drugi rzut:");
+        dice = secondThrow.reroll(dice);
+    }
+}
