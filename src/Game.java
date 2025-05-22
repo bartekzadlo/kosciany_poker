69305693e4c01cdffc142f685fc0fa8cc9f69733
Admin <Admin@DESKTOP-LC407O9>
2025-05-22 21:15:33 +0200

    Dodanie rozróżnianie wartości par, stritu oraz braku układu

--- src/Game.java
+++ src/Game.java
@@ -18,13 +18,13 @@ public class Game {
         printDice(player2.getDice());
         System.out.println("Układ: " + ScoreEvaluator.evaluate(player2.getDice()));
 
-        // Wyświetlanie kości przed drugim rzutem gracza 1 (zgodnie z oryginałem)
+        // Drugi rzut gracza 1
         System.out.println('\n' + player1.getName() + ":");
         printDice(player1.getDice());
         player1.secondThrow(secondThrow);
         System.out.println("Układ po drugim rzucie: " + ScoreEvaluator.evaluate(player1.getDice()));
 
-        // Wyświetlanie kości przed drugim rzutem gracza 2 (zgodnie z oryginałem)
+        // Drugi rzut gracza 2
         System.out.println('\n' + player2.getName() + ":");
         printDice(player2.getDice());
         player2.secondThrow(secondThrow);
@@ -37,31 +37,23 @@ public class Game {
         System.out.println(player1.getName() + ":");
         printDice(player1.getDice());
         String hand1 = ScoreEvaluator.evaluate(player1.getDice());
-        int strength1 = ScoreEvaluator.evaluateStrength(player1.getDice());
+        ScoreEvaluator.HandDetail detail1 = ScoreEvaluator.evaluateDetailed(player1.getDice());
         System.out.println("Układ końcowy: " + hand1);
 
         System.out.println(player2.getName() + ":");
         printDice(player2.getDice());
         String hand2 = ScoreEvaluator.evaluate(player2.getDice());
-        int strength2 = ScoreEvaluator.evaluateStrength(player2.getDice());
+        ScoreEvaluator.HandDetail detail2 = ScoreEvaluator.evaluateDetailed(player2.getDice());
         System.out.println("Układ końcowy: " + hand2);
 
         System.out.println("\nZwycięzca:");
-        if (strength1 > strength2) {
+        int result = detail1.compareTo(detail2);
+        if (result > 0) {
             System.out.println(player1.getName() + " wygrywa!");
-        } else if (strength2 > strength1) {
+        } else if (result < 0) {
             System.out.println(player2.getName() + " wygrywa!");
         } else {
-            // Remis – dodatkowo porównujemy najwyższą kość
-            int max1 = maxValue(player1.getDice());
-            int max2 = maxValue(player2.getDice());
-            if (max1 > max2) {
-                System.out.println(player1.getName() + " wygrywa dzięki wyższej kości (" + max1 + " vs " + max2 + ")!");
-            } else if (max2 > max1) {
-                System.out.println(player2.getName() + " wygrywa dzięki wyższej kości (" + max2 + " vs " + max1 + ")!");
-            } else {
-                System.out.println("Remis!");
-            }
+            System.out.println("Remis!");
         }
     }
 
@@ -70,15 +62,5 @@ public class Game {
             System.out.print(value + " ");
         }
         System.out.println();
-    }
-
-    private static int maxValue(int[] dice) {
-        int max = 0;
-        for (int die : dice) {
-            if (die > max) {
-                max = die;
-            }
-        }
-        return max;
     }
 }
--- src/ScoreEvaluator.java
+++ src/ScoreEvaluator.java
@@ -34,7 +34,6 @@ public class ScoreEvaluator {
         if (pairs == 2) return "Dwie Pary";
         if (pairs == 1) return "Para";
 
-        // Zwracamy najwyższą liczbę oczek, jeśli nie ma żadnego układu
         int maxValue = Arrays.stream(dice).max().orElse(0);
         return "Najwyższa liczba oczek: " + maxValue;
     }
@@ -70,6 +69,104 @@ public class ScoreEvaluator {
         if (pairs == 1) return 1;
 
         return 0; // tylko najwyższa kość
+    }
+
+    public static class HandDetail implements Comparable<HandDetail> {
+        int strength;        // np. 2 = Dwie Pary, 3 = Trójka itd.
+        int primaryValue;    // wartość głównego układu (np. para szóstek -> 6)
+        int[] tiebreakers;   // pozostałe kości (malejąco)
+
+        public HandDetail(int strength, int primaryValue, int[] tiebreakers) {
+            this.strength = strength;
+            this.primaryValue = primaryValue;
+            this.tiebreakers = tiebreakers;
+        }
+
+        @Override
+        public int compareTo(HandDetail o) {
+            if (this.strength != o.strength) return Integer.compare(this.strength, o.strength);
+            if (this.primaryValue != o.primaryValue) return Integer.compare(this.primaryValue, o.primaryValue);
+            for (int i = 0; i < Math.min(this.tiebreakers.length, o.tiebreakers.length); i++) {
+                if (this.tiebreakers[i] != o.tiebreakers[i])
+                    return Integer.compare(this.tiebreakers[i], o.tiebreakers[i]);
+            }
+            return 0;
+        }
+    }
+
+    public static HandDetail evaluateDetailed(int[] dice) {
+        int[] counts = new int[7];
+        for (int die : dice) counts[die]++;
+
+        int strength = 0;
+        int primary = 0;
+
+        // Sprawdź kombinacje od najwyższych
+        for (int i = 6; i >= 1; i--) {
+            if (counts[i] == 5) { strength = 8; primary = i; break; } // Poker
+            if (counts[i] == 4) { strength = 7; primary = i; break; } // Kareta
+            if (counts[i] == 3) {
+                for (int j = 6; j >= 1; j--) {
+                    if (j != i && counts[j] >= 2) {
+                        strength = 6; primary = i; break; // Full
+                    }
+                }
+                if (strength == 0) {
+                    strength = 3; primary = i; // Trójka
+                }
+                break;
+            }
+        }
+
+        // Dwie pary / para
+        if (strength == 0) {
+            int firstPair = 0, secondPair = 0;
+            for (int i = 6; i >= 1; i--) {
+                if (counts[i] == 2) {
+                    if (firstPair == 0) firstPair = i;
+                    else if (secondPair == 0) secondPair = i;
+                }
+            }
+            if (firstPair > 0 && secondPair > 0) {
+                strength = 2;
+                primary = Math.max(firstPair, secondPair);
+            } else if (firstPair > 0) {
+                strength = 1;
+                primary = firstPair;
+            }
+        }
+
+        // Strity
+        if (strength == 0) {
+            boolean small = checkStraight(dice, 1);
+            boolean big = checkStraight(dice, 2);
+            if (big) {
+                strength = 5; primary = 5;
+            } else if (small) {
+                strength = 4; primary = 4;
+            }
+        }
+
+        // Brak układu – najwyższa wartość
+        if (strength == 0) {
+            strength = 0;
+            primary = Arrays.stream(dice).max().orElse(0);
+        }
+
+        // Tiebreakery – reszta oczek (pomijamy oczka układu, jeśli istotne)
+        int[] tiebreakers = new int[5];
+        int idx = 0;
+        for (int i = 6; i >= 1; i--) {
+            for (int j = 0; j < counts[i]; j++) {
+                if (i != primary || strength == 0) {
+                    tiebreakers[idx++] = i;
+                    if (idx == 5) break;
+                }
+            }
+            if (idx == 5) break;
+        }
+
+        return new HandDetail(strength, primary, Arrays.copyOf(tiebreakers, idx));
     }
 
     private static boolean checkStraight(int[] dice, int start) {
