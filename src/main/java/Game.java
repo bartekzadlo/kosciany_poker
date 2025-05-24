b5efbc5760ce69a0c5257c9f9e7f678e5294bbd6
Admin <Admin@DESKTOP-LC407O9>
2025-05-24 03:29:21 +0200

    Dodanie możliwości podbijania stawki po pierwszym rzucie przez obu graczy oraz możliwości poddania partii

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -32,44 +32,76 @@ public class Game {
             FirstThrow firstThrow = new FirstThrow();
             SecondThrow secondThrow = new SecondThrow();
 
-            // Pierwsze rzuty
+            int currentStake = stake; // aktualna stawka (może być podbita)
+            boolean someoneFolded = false;
+            Player foldWinner = null; // kto wygrał, gdy ktoś spasował
+
+            // --- Runda pierwszego gracza ---
             player1.firstThrow(firstThrow);
             System.out.println(player1.getName() + ":");
             printDice(player1.getDice());
             System.out.println("Układ: " + ScoreEvaluator.evaluate(player1.getDice()));
 
+            // --- Runda pierwszego rzutu drugiego gracza ---
             player2.firstThrow(firstThrow);
             System.out.println(player2.getName() + ":");
             printDice(player2.getDice());
             System.out.println("Układ: " + ScoreEvaluator.evaluate(player2.getDice()));
 
-            // Drugie rzuty
-            System.out.println("\n" + player1.getName() + ":");
-            printDice(player1.getDice());
-            player1.secondThrow(secondThrow);
-            System.out.println("Układ po drugim rzucie: " + ScoreEvaluator.evaluate(player1.getDice()));
+            // Teraz pytanie o podbicie stawki, najpierw dla pierwszego gracza
+            currentStake = tryRaise(scanner, player1, player2, stake, currentStake);
+            if (currentStake == -1) { // oznacza, że drugi gracz spasował
+                someoneFolded = true;
+                foldWinner = player1;
+            }
 
-            System.out.println("\n" + player2.getName() + ":");
-            printDice(player2.getDice());
-            player2.secondThrow(secondThrow);
-            System.out.println("Układ po drugim rzucie: " + ScoreEvaluator.evaluate(player2.getDice()));
+            // Jeśli nikt nie spasował, pytanie o podbicie dla drugiego gracza
+            if (!someoneFolded) {
+                currentStake = tryRaise(scanner, player2, player1, stake, currentStake);
+                if (currentStake == -1) { // oznacza, że pierwszy gracz spasował
+                    someoneFolded = true;
+                    foldWinner = player2;
+                }
+            }
 
-            // Porównanie wyników
-            ScoreEvaluator.HandDetail detail1 = ScoreEvaluator.evaluateDetailed(player1.getDice());
-            ScoreEvaluator.HandDetail detail2 = ScoreEvaluator.evaluateDetailed(player2.getDice());
+            if (!someoneFolded) {
+                // --- Drugi rzut obu graczy ---
+                System.out.println("\n" + player1.getName() + ":");
+                printDice(player1.getDice());
+                player1.secondThrow(secondThrow);
+                System.out.println("Układ po drugim rzucie: " + ScoreEvaluator.evaluate(player1.getDice()));
 
-            System.out.println("\nZwycięzca:");
-            int result = detail1.compareTo(detail2);
-            if (result > 0) {
-                System.out.println(player1.getName() + " wygrywa i zdobywa " + stake + " zł!");
-                player1.addMoney(stake);
-                player2.subtractMoney(stake);
-            } else if (result < 0) {
-                System.out.println(player2.getName() + " wygrywa i zdobywa " + stake + " zł!");
-                player2.addMoney(stake);
-                player1.subtractMoney(stake);
+                System.out.println("\n" + player2.getName() + ":");
+                printDice(player2.getDice());
+                player2.secondThrow(secondThrow);
+                System.out.println("Układ po drugim rzucie: " + ScoreEvaluator.evaluate(player2.getDice()));
+
+                // Porównanie wyników
+                ScoreEvaluator.HandDetail detail1 = ScoreEvaluator.evaluateDetailed(player1.getDice());
+                ScoreEvaluator.HandDetail detail2 = ScoreEvaluator.evaluateDetailed(player2.getDice());
+
+                System.out.println("\nZwycięzca:");
+                int result = detail1.compareTo(detail2);
+                if (result > 0) {
+                    System.out.println(player1.getName() + " wygrywa i zdobywa " + currentStake + " zł!");
+                    player1.addMoney(currentStake);
+                    player2.subtractMoney(currentStake);
+                } else if (result < 0) {
+                    System.out.println(player2.getName() + " wygrywa i zdobywa " + currentStake + " zł!");
+                    player2.addMoney(currentStake);
+                    player1.subtractMoney(currentStake);
+                } else {
+                    System.out.println("Remis! Nikt nie traci pieniędzy.");
+                }
             } else {
-                System.out.println("Remis! Nikt nie traci pieniędzy.");
+                // Gdy ktoś spasował
+                System.out.println("\n" + foldWinner.getName() + " wygrywa, ponieważ przeciwnik spasował.");
+                foldWinner.addMoney(currentStake);
+                if (foldWinner == player1) {
+                    player2.subtractMoney(currentStake);
+                } else {
+                    player1.subtractMoney(currentStake);
+                }
             }
 
             // Sprawdź czy ktoś nie zbankrutował
@@ -105,6 +137,53 @@ public class Game {
             }
         }
     }
+
+    // Metoda do obsługi podbicia stawki
+    // Zwraca:
+    //  - nową stawkę jeśli zaakceptowano podbicie lub brak podbicia
+    //  - -1 jeśli przeciwnik spasował
+    private static int tryRaise(Scanner scanner, Player raiser, Player opponent, int initialStake, int currentStake) {
+        System.out.println("\n" + raiser.getName() + ", czy chcesz podbić stawkę? (aktualna stawka: " + currentStake + " zł)");
+        System.out.println("1. Tak");
+        System.out.println("2. Nie");
+        int choice = scanner.nextInt();
+
+        if (choice == 1) {
+            int maxRaise = Math.min(raiser.getMoney() - currentStake, opponent.getMoney() - currentStake);
+            if (maxRaise <= 0) {
+                System.out.println("Conajmniej jeden z graczy, nie ma wystarczającej ilości pieniędzy.");
+                return currentStake;
+            }
+            int raiseAmount;
+            while (true) {
+                System.out.print("Podaj kwotę podbicia (max " + maxRaise + " zł): ");
+                raiseAmount = scanner.nextInt();
+
+                if (raiseAmount > 0 && raiseAmount <= maxRaise) {
+                    break;
+                } else {
+                    System.out.println("Nieprawidłowa kwota podbicia.");
+                }
+            }
+
+            int newStake = currentStake + raiseAmount;
+
+            System.out.println(opponent.getName() + ", czy akceptujesz podbicie do " + newStake + " zł?");
+            System.out.println("1. Akceptuję");
+            System.out.println("2. Puszczam (poddaję się)");
+
+            int opponentChoice = scanner.nextInt();
+            if (opponentChoice == 1) {
+                System.out.println(opponent.getName() + " zaakceptował podbicie.");
+                return newStake;
+            } else {
+                System.out.println(opponent.getName() + " spasował.");
+                return -1; // przeciwnik spasował
+            }
+        }
+        return currentStake; // brak podbicia
+    }
+
 
     private static void printDice(int[] dice) {
         for (int value : dice) {
